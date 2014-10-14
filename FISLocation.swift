156a1aa7eaaeec37cf6cdcc824db0281660996f0
zdrossman <zachary.drossman@gmail.com>
2014-10-14 16:12:18 -0400

    Updated for test_runner mistake

---
+++ FISLocation.swift
@@ -0,0 +1,72 @@
+//
+//  FISLocation.swift
+//  locationTrivia-Objects
+//
+//  Created by Zachary Drossman on 10/13/14.
+//  Copyright (c) 2014 Joe Burgess. All rights reserved.
+//
+
+import UIKit
+
+class FISLocation: NSObject {
+    var name: String = ""
+    var latitude: Double = 0
+    var longitude: Double = 0
+    var trivia: [FISTrivia] = []
+
+    override init() {}
+    
+    init(_ name: String, latitude: Double, longitude: Double)
+    {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.name = name;
+    }
+    
+
+    
+    func shortenedNameToLength(length: Int) -> String
+    {
+        if length < 0
+        {
+            return self.name;
+        }
+        
+        let substringIndex:String.Index = advance(self.name.startIndex, 5)
+        
+        return name.substringToIndex(substringIndex)
+    }
+    
+    func verifyLocation() -> Bool
+    {
+        
+        let validLatitude : Bool = latitude >= -90 && latitude <= 90
+        let validLongitude : Bool = longitude >= -180 && longitude <= 180
+        
+        if countElements(self.name) > 0 && validLongitude && validLatitude
+        {
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    func topTrivia() -> FISTrivia?
+    {
+        if self.trivia.count == 0
+        {
+            return nil
+        }
+        
+        var topTrivia : FISTrivia = self.trivia[0]
+        for trivium in self.trivia
+        {
+            if topTrivia.likes < trivium.likes
+            {
+                topTrivia = trivium
+            }
+        }
+        
+        return topTrivia
+    }
+}
---
+++ FISTrivia.swift
@@ -0,0 +1,22 @@
+//
+//  FISTrivia.swift
+//  locationTrivia-Objects
+//
+//  Created by Zachary Drossman on 10/13/14.
+//  Copyright (c) 2014 Joe Burgess. All rights reserved.
+//
+
+import UIKit
+
+class FISTrivia: NSObject {
+    var content : String = ""
+    var likes : Int = 0
+    
+    init (content : String, likes : Int)
+    {
+        self.content = content;
+        self.likes = likes;
+    }
+    
+    override init() {}
+}
